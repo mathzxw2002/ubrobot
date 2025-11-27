d645a97da1756e5c95fbcd6fe16583d112afcacf
Xiaowei Zhao <mathzxw2002@gmail.com>
2025-11-27 15:16:04 +0800

    chatbot for robot

---
+++ chatbot.py
@@ -0,0 +1,19 @@
+import gradio as gr
+import random
+import time
+
+with gr.Blocks() as demo:
+    chatbot = gr.Chatbot()
+    msg = gr.Textbox()
+    clear = gr.ClearButton([msg, chatbot])
+
+    def respond(message, chat_history):
+        bot_message = message #random.choice(["How are you?", "Today is a great day", "I'm very hungry"])
+        chat_history.append({"role": "user", "content": message})
+        chat_history.append({"role": "assistant", "content": bot_message})
+        time.sleep(2)
+        return "", chat_history
+
+    msg.submit(respond, [msg, chatbot], [msg, chatbot])
+
+demo.launch(server_name="0.0.0.0", server_port=7861)

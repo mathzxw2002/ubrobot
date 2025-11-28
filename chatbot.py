7597ec0ad8f4d2571356181747d267990058f4e4
mathzxw2002 <mathzxw2002@gmail.com>
2025-11-28 09:18:21 +0800

    move cosmos infer to server side

--- chatbot.py
+++ chatbot.py
@@ -6,14 +6,31 @@ from pathlib import Path
 
 import qwen_vl_utils
 import transformers
+import json
+import requests
+
 
 ROOT = Path(__file__).parents[2]
 SEPARATOR = "-" * 20
 
+def cosmos_reason1_infer(image_bytes, instruction, url='http://192.168.18.230:5802/eval_cosmos_reason1'):
 
-#CosmosReason1Infer cosmos_infer
+    instruction = "trun around to the bag side"
+    data = {"ins": instruction}
+    json_data = json.dumps(data)
 
-from cosmos_reason1_infer import CosmosReason1Infer
+    files = {
+        'image': ('rgb_image', image_bytes, 'image/jpeg'),
+    }
+    start = time.time()
+    response = requests.post(url, files=files, data={'json': json_data}, timeout=100)
+    print(f"response {response.text}")
+    '''http_idx += 1
+    if http_idx == 0:
+        first_running_time = time.time()
+    print(f"idx: {http_idx} after http {time.time() - start}")'''
+
+    return json.loads(response.text)
 
 def create_chatbot_interface() -> gr.Blocks:
     """
@@ -27,7 +44,9 @@ def create_chatbot_interface() -> gr.Blocks:
         clear = gr.ClearButton([msg, chatbot])
 
         def respond(message, chat_history):
-            resut_str = cosmos_infer.infer_once("image_path", message)
+            image_bytes = None
+            resut_str = cosmos_reason1_infer(image_bytes, message)
+            
             bot_message = resut_str
             chat_history.append({"role": "user", "content": message})
             chat_history.append({"role": "assistant", "content": bot_message})
@@ -42,9 +61,6 @@ def create_chatbot_interface() -> gr.Blocks:
 if __name__ == "__main__":
     demo = create_chatbot_interface()
     
-    model_name = "/home/sany/.cache/modelscope//hub/models/nv-community/Cosmos-Reason1-7B"
-    cosmos_infer = CosmosReason1Infer(model_name)
-
     demo.launch(
         server_name="0.0.0.0",
         server_port=7862,

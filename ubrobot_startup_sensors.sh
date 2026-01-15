33f418ed198ef973a2222b944e1bccad10370f5c
mathzxw2002 <mathzxw2002@gmail.com>
2026-01-15 09:23:51 +0800

    m

--- ubrobot_startup_sensors.sh
+++ ubrobot_startup_sensors.sh
@@ -1,17 +1,12 @@
 # ROS 
 source /opt/ros/noetic/setup.bash
-
 source ./ros_depends_ws/devel_isolated/setup.bash
 
 export LD_PRELOAD=/lib/aarch64-linux-gnu/libgomp.so.1
 
 export DASHSCOPE_API_KEY="sk-479fdd23120c4201bff35a107883c7c3"
-# 配置半精度模式（布尔值建议用小写或引号包裹，避免Shell解析问题）
 export IS_HALF="True"
 
-#bash ros_depends_ws/src/piper_ros/can_activate.sh
-# arm 336222070923 
-# head 419522070679
+bash ros_depends_ws/src/piper_ros/can_activate.sh
+# head realsense d435i camera sn: 419522070679
 roslaunch ros_depends_ws/src/rtabmap_ros/rtabmap_examples/launch/ubrobot.launch --screen
-
-#python3 src/chat_ui/app.py

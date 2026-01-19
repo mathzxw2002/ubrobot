9321bbce87ba1045fc88d277eb3e96977ab676d1
mathzxw2002 <mathzxw2002@gmail.com>
2026-01-19 08:33:52 +0800

    auto build script for this project

--- install.sh
+++ install.sh
@@ -13,27 +13,28 @@ sudo apt install -y liborocos-kdl-dev libeigen3-dev libboost-all-dev
 
 # install ubrobot
 cd "$HOME/ubrobot"
-pip install -e .
+#pip install -e .
 
 # pytracik
+sudo apt install libboost-all-dev libeigen3-dev liborocos-kdl-dev libnlopt-dev libnlopt-cxx-dev
 cd $build_dir
 git clone https://github.com/chenhaox/pytracik.git
 cd pytracik
-#pip install -r requirements.txt
 python setup_linux.py install
 
 
 # cyclonedds and unitree pysdk
-#cd ~
-#git clone https://github.com/eclipse-cyclonedds/cyclonedds -b releases/0.10.x
-#cd cyclonedds && mkdir build install && cd build
-#cmake .. -DCMAKE_INSTALL_PREFIX=../install
-#cmake --build . --target install
-#export CYCLONEDDS_HOME="~/cyclonedds/install"
+cd ~
+rm -rf cyclonedds
+git clone https://github.com/eclipse-cyclonedds/cyclonedds -b releases/0.10.x
+cd cyclonedds && mkdir build install && cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=../install
+cmake --build . --target install
+export CYCLONEDDS_HOME="~/cyclonedds/install"
 
-#echo $CYCLONEDDS_HOME
+cd $build_dir
+git clone https://github.com/unitreerobotics/unitree_sdk2_python.git
+cd unitree_sdk2_python
+pip3 install -e .
 
-#cd ~
-#git clone https://github.com/unitreerobotics/unitree_sdk2_python.git
-#cd unitree_sdk2_python
-#pip3 install -e .
+

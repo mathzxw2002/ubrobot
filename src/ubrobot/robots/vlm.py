7852ec2d118b9228357bd879eb8f2c3c1b1a7721
mathzxw2002 <mathzxw2002@gmail.com>
2026-02-19 15:30:02 +0800

    Remove hardcoded API key from RobotVLM
    Removed hardcoded API key from RobotVLM class.

--- src/ubrobot/robots/vlm.py
+++ src/ubrobot/robots/vlm.py
@@ -19,7 +19,7 @@ class RobotVLM:
     def __init__(self, api_key = None, base_url = "https://dashscope.aliyuncs.com/compatible-mode/v1", url = "http://192.168.18.230:5802/eval_cosmos_reason1"):
         self.url = url
         self.client = OpenAI(
-            api_key="sk-479fdd23120c4201bff35a107883c7c3",
+            api_key="",
             base_url=base_url,
         )            
     

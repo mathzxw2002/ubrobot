53a7922899b935474fa5917ed888fdabac92e4f5
Xiaowei Zhao <mathzxw2002@gmail.com>
2026-06-11 15:27:56 +0800

    Make detection relay timestamps monotonic

--- ros_depends_ws/src/emos_bringup/emos_bringup/fix_detection_header.py
+++ ros_depends_ws/src/emos_bringup/emos_bringup/fix_detection_header.py
@@ -11,6 +11,15 @@ def _has_stamp(stamp):
     return stamp.sec != 0 or stamp.nanosec != 0
 
 
+def _stamp_to_ns(stamp):
+    return stamp.sec * 1_000_000_000 + stamp.nanosec
+
+
+def _set_stamp_from_ns(stamp, stamp_ns):
+    stamp.sec = stamp_ns // 1_000_000_000
+    stamp.nanosec = stamp_ns % 1_000_000_000
+
+
 class DetectionHeaderFixer(Node):
     """Make the top-level detection header match the RGB-D payload header."""
 
@@ -29,6 +38,7 @@ class DetectionHeaderFixer(Node):
         )
 
         self._publisher = self.create_publisher(Detections2D, output_topic, qos)
+        self._last_stamp_ns = None
         self.create_subscription(Detections2D, input_topic, self._callback, qos)
         self.get_logger().info(
             f"Republishing detections from {input_topic} to {output_topic}"
@@ -44,6 +54,13 @@ class DetectionHeaderFixer(Node):
             msg.header.frame_id = msg.image.header.frame_id
         elif msg.depth.header.frame_id:
             msg.header.frame_id = msg.depth.header.frame_id
+
+        if _has_stamp(msg.header.stamp):
+            stamp_ns = _stamp_to_ns(msg.header.stamp)
+            if self._last_stamp_ns is not None and stamp_ns <= self._last_stamp_ns:
+                stamp_ns = self._last_stamp_ns + 1
+                _set_stamp_from_ns(msg.header.stamp, stamp_ns)
+            self._last_stamp_ns = stamp_ns
 
         self._publisher.publish(msg)
 

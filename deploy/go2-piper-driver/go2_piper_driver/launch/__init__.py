249c18f069218ff21b4da9d57a11fce306fd5962
Xiaowei Zhao <mathzxw2002@gmail.com>
2026-08-07 10:49:53 +0800

    fix: colcon build of go2-piper-driver (launch inside package)

---
+++ deploy/go2-piper-driver/go2_piper_driver/launch/__init__.py
@@ -0,0 +1,3 @@
+"""Launch files for the go2-piper-driver package (installed as package data)."""
+
+__version__ = "0.1.0"
--- deploy/go2-piper-driver/launch/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Package metadata for the Go2+Piper driver bring-up launch files."""
-
-__version__ = "0.1.0"
